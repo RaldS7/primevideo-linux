c2f8ba5cea9fc40c142300c27d9434f520c03e69
nxjosephofficial <61632416+nxjosephofficial@users.noreply.github.com>
2022-04-07 18:16:34 +0300

    Update arch-install.sh

--- arch/arch-install.sh
+++ arch/arch-install.sh
@@ -8,6 +8,7 @@ Install wine-staging and winetricks,
 Open winecfg, 'you should change Windows Version to "Windows 7"',
 Install dxvk with winetricks,
 Go to Downloads, execute the exe file with wine.
+NOTE: Click to Install in Wine Mono Installer window
 """
 echo "Do you want to continue?"
 echo "Type yes or no "
@@ -17,15 +18,13 @@ if [[ $answer == "yes" || $answer == "YES" ]]; then
 echo
 echo "Script executed."
 sudo cp /etc/pacman.conf /etc/pacman.conf.bak
-#echo [multilib] >> /etc/pacman.conf
 echo "[multilib]" | sudo tee -a /etc/pacman.conf > /dev/null
-#echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf > /dev/null
 sudo pacman -Sy wine-staging winetricks
 winecfg
 winetricks -q dxvk
-cd ~/Downloads/
-wine *.exe
+curl --location --request GET 'https://www.slimjet.com/chrome/download-chrome.php?file=files%2F90.0.4430.72%2FChromeStandaloneSetup.exe' --output /home/$USER/Downloads/Chrome.exe
+wine /home/$USER/Downloads/Chrome.exe
 elif [[ $answer == "no" || $anser == "NO" ]]; then
 echo
 echo "Script didn't executed and exiting."
